062a82ebf1cbff73066ebfd527a632f04d71db51
NatSI <Silyukova1999@mail.ru>
2023-11-29 20:19:05 +0300

    Image, title митапа с исп. возможностей SFC

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+  props: {
+    title: {
+      type: String,
+    },
+    image: {
+      type: String,
+    }
+  },
+  computed: {
+    getUrl() {
+      return this.image ? `url('${this.image}')` : 'var(--default-cover)';
+    }
+  }
 };
 </script>
 
@@ -14,8 +27,7 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(getUrl);
   /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
   display: flex;
   flex-direction: column;
