31f290c7361d84fdf189beb9977a98498e8c4a49
NatSI <Silyukova1999@mail.ru>
2024-01-04 13:54:39 +0300

    Реализован компонент CounterButton

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,35 @@
 <template>
-  <button></button>
+  <button @click="updateCount">{{ localCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+  emits: ['update:count'],
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
+  data() {
+    return {
+      localCount: this.count,
+    }
+  },
+  methods: {
+    updateCount() {
+      this.localCount++;
+      this.$emit('update:count', this.localCount);
+    }
+  },
+  watch: {
+    count: {
+      immediate: true,
+      handler(newValue) {
+        this.localCount = newValue;
+      },
+    }
+  }
 };
 </script>
