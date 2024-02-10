4833caa5c6309eee2f7de8b066a57f844845d410
NatSI <Silyukova1999@mail.ru>
2024-02-10 13:42:21 +0300

    Реализована вспомогательная функция реактивности debouncedRef

--- 08-reactivity/04-debouncedRef/utils/debouncedRef.js
+++ 08-reactivity/04-debouncedRef/utils/debouncedRef.js
@@ -8,7 +8,11 @@ import debounce from 'lodash/debounce';
  * @returns {Ref<T>} - Новый ref, обновляющийся с debounce при обновлении исходного ref-а
  */
 export function debouncedRef(source, wait) {
-  const debounced = ref(undefined); // ...
-  // ...
+  const debounced = ref(source.value);
+  watch(source,
+    debounce((newValue) => {
+      debounced.value = newValue;
+    }, wait)
+  )
   return debounced;
 }
