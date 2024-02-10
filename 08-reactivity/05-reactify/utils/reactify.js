752593c92eca875ec2ef6f25a8282de15117e9a0
NatSI <Silyukova1999@mail.ru>
2024-02-10 13:58:07 +0300

    Реализована функция высшего порядка reactify

--- 08-reactivity/05-reactify/utils/reactify.js
+++ 08-reactivity/05-reactify/utils/reactify.js
@@ -1,4 +1,4 @@
-import { computed } from 'vue';
+import { computed, unref, ref } from 'vue';
 
 /**
  * @template T
@@ -6,6 +6,7 @@ import { computed } from 'vue';
  * @returns {function(...[*]): ComputedRef<T>} - Функция вычисления от ref-ов, возвращающая вычисляемое значение computed
  */
 export function reactify(func) {
-  // ...
-  // return () => computed(() => {});
+  return (...args) => computed(() => {
+    return func(...args.map(arg => unref(arg)));
+  });
 }
